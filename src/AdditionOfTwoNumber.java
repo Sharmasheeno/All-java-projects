4f90b2372156dc30ed8750f64c9db7da6adcded7
Sharmasheeno <135059937+Sharmasheeno@users.noreply.github.com>
2024-02-22 17:16:18 +0300

    Secondlesson

---
+++ src/AdditionOfTwoNumber.java
@@ -0,0 +1,11 @@
+public class AdditionOfTwoNumber {
+    public static void main (String[]args){
+        //Delclaring variables
+        double a,b,c;
+        a=10;
+        b=20;
+        c=a+b;
+        //display the Sum of all Numbers
+        System.out.println("the sum of all numbers is"+c);
+    }
+}
