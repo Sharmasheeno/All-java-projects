0bfdbae957db49b8952cb06430fce1d715dbf967
Sharmasheeno <135059937+Sharmasheeno@users.noreply.github.com>
2024-03-03 23:06:20 +0300

    IncrementAndDecrement

---
+++ src/IncrementAndDecrement.java
@@ -0,0 +1,30 @@
+ import java.util.*;
+public class IncrementAndDecrement {
+    public static void main(String[] args) {
+   int i = 4;
+     int j = ++i;
+     System.out.println(j);
+      System.out.println(i);
+   /*     int j = 5;
+        int k = j++;
+        System.out.println(j);
+        System.out.println(k);
+        int i = 5;
+        int k = ++i + (++i*i);
+        System.out.println(i);
+        System.out.println(k);
+        int h = 10;
+        int v = 10 * ++h * h-- +(++h);*/
+//        double d = 3;
+//        System.out.println(d);
+//
+//        int i = 0;
+//        i+= 12.5;
+//        System.out.println(i);d
+            double o = 0.05;
+            double u = o * 45;
+            System.out.println(u);
+            System.out.println(o);
+        }
+    }
+
