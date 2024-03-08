370e8a9cf13df9a9225a7a615604f00f85e93f34
Sharmasheeno <135059937+Sharmasheeno@users.noreply.github.com>
2024-03-08 20:39:12 +0300

    AdditionQuiz

--- src/AdditionQuiz.java
+++ src/AdditionQuiz.java
@@ -1,3 +1,13 @@
+import java.util.Scanner;
+
 public class AdditionQuiz {
-    
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        int number1 = (int) (System.currentTimeMillis() % 10);
+        int number2 = (int) (System.currentTimeMillis() / 10 % 10);
+        System.out.print("What is the Addition of:" + number1 + "+" + number2);
+        int answer = input.nextInt();
+
+        System.out.println(number1 + number2 + " = " + answer+" "+ "is " +(number1+number2==answer));
+    }
 }
